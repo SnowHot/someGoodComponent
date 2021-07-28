ea3da267fe345a616f258185e15b1b8d4ce70930
niupengzhi <947277911@qq.com>
2021-07-28 10:02:07 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { isSortArray } = require("./fc/isSortArray");
-const { flat } = require("./fc/flat");
-const { is } = require("./fc/isArray");
+const { isSortArray } = require("./fc/array/isSortArray");
+const { flat } = require("./fc/array/flat");
+const { is } = require("./fc/array/isArray");
 const tap = require("./fc/tap");
 
 module.exports = { isSortArray, flat, is, tap };
